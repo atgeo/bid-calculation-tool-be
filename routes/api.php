8ac3933b34e9c8c8a0e58245ceb63a6b4fd2299c
Georges Kmeid <georgesk117@gmail.com>
2024-09-18 20:39:51 -0400

    Add login endpoint and require token to hit calculate-price

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,18 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PriceCalculationController;
 
+Route::controller(RegisterController::class)->group(function () {
+    //Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/calculate-price', [PriceCalculationController::class, 'calculate']);
+Route::post('/calculate-price', [PriceCalculationController::class, 'calculate'])->middleware('auth:sanctum');
