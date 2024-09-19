1d2cebe2f87e62e2d21b8f88d80f0076f948a917
Georges Kmeid <georgesk117@gmail.com>
2024-09-19 16:46:10 -0400

    Refactor CalculateTest and add one more test

--- tests/Unit/CalculateTest.php
+++ tests/Unit/CalculateTest.php
@@ -11,11 +11,9 @@ use PHPUnit\Framework\TestCase;
 
 class CalculateTest extends TestCase
 {
-    protected PriceCalculatorService $priceCalculator;
+    private PriceCalculatorService $priceCalculator;
 
-    protected MockObject $pricingRuleRepoMock;
-
-    protected MockObject $pricingRuleMock;
+    private MockObject $pricingRuleRepoMock;
 
     protected function setUp(): void
     {
@@ -23,8 +21,6 @@ class CalculateTest extends TestCase
 
         // Create a mock for the PricingRuleRepository
         $this->pricingRuleRepoMock = $this->createMock(PricingRuleRepository::class);
-
-        $this->pricingRuleMock = $this->createMock(PricingRule::class);
 
         // Instantiate the PriceCalculatorService with the mocked repository
         $this->priceCalculator = new PriceCalculatorService($this->pricingRuleRepoMock);
@@ -35,25 +31,20 @@ class CalculateTest extends TestCase
         $vehiclePrice = 57;
         $vehicleType = VehicleType::COMMON->value;
 
-        $pricingRuleCommon = PricingRule::factory()->state([
-            'vehicle_type' => $vehicleType,
-            'buyer_fee_percentage' => 0.10,
-            'min_buyer_fee' => 10,
-            'max_buyer_fee' => 50,
-            'seller_fee_percentage' => 0.02,
-            'association_fee' => [
+        $this->mockPricingRule(
+            $vehicleType,
+            0.10,
+            10,
+            50,
+            0.02,
+            [
                 '1_to_500' => 5,
                 '501_to_1000' => 10,
                 '1001_to_3000' => 15,
                 'over_3000' => 20,
             ],
-            'storage_fee' => 100.00
-        ])->make();
-
-        $this->pricingRuleRepoMock->expects($this->once())
-            ->method('getPricingRules')
-            ->with($vehicleType)
-            ->willReturn($pricingRuleCommon);
+            100.00
+        );
 
         $result = $this->priceCalculator->calculateFeesAndTotalPrice($vehiclePrice, $vehicleType);
 
@@ -69,25 +60,20 @@ class CalculateTest extends TestCase
         $vehiclePrice = 1800;
         $vehicleType = VehicleType::LUXURY->value;
 
-        $pricingRuleLuxury = PricingRule::factory()->state([
-            'vehicle_type' => $vehicleType,
-            'buyer_fee_percentage' => 0.10,
-            'min_buyer_fee' => 25,
-            'max_buyer_fee' => 200,
-            'seller_fee_percentage' => 0.04,
-            'association_fee' => [
+        $this->mockPricingRule(
+            $vehicleType,
+            0.10,
+            25,
+            200,
+            0.04,
+            [
                 '1_to_500' => 5,
                 '501_to_1000' => 10,
                 '1001_to_3000' => 15,
                 'over_3000' => 20,
             ],
-            'storage_fee' => 100.00
-        ])->make();
-
-        $this->pricingRuleRepoMock->expects($this->once())
-            ->method('getPricingRules')
-            ->with($vehicleType)
-            ->willReturn($pricingRuleLuxury);
+            100.00
+        );
 
         $result = $this->priceCalculator->calculateFeesAndTotalPrice($vehiclePrice, $vehicleType);
 
@@ -96,5 +82,60 @@ class CalculateTest extends TestCase
         $this->assertEquals(72, $result['sellerFee']);
         $this->assertEquals(15, $result['associationFee']);
         $this->assertEquals(100, $result['storageFee']);
+    }
+
+    public function test_calculate_function_luxury_1000000(): void
+    {
+        $vehiclePrice = 1000000;
+        $vehicleType = VehicleType::LUXURY->value;
+
+        $this->mockPricingRule(
+            $vehicleType,
+            0.10,
+            25,
+            200,
+            0.04,
+            [
+                '1_to_500' => 5,
+                '501_to_1000' => 10,
+                '1001_to_3000' => 15,
+                'over_3000' => 20,
+            ],
+            100.00
+        );
+
+        $result = $this->priceCalculator->calculateFeesAndTotalPrice($vehiclePrice, $vehicleType);
+
+        $this->assertEquals(1040320, $result['totalPrice']);
+        $this->assertEquals(200, $result['buyerFee']);
+        $this->assertEquals(40000, $result['sellerFee']);
+        $this->assertEquals(20, $result['associationFee']);
+        $this->assertEquals(100, $result['storageFee']);
+    }
+
+    private function mockPricingRule(
+        string $vehicleType,
+        float  $buyerFeePercentage,
+        float  $minBuyerFee,
+        float  $maxBuyerFee,
+        float  $sellerFeePercentage,
+        array  $associationFee,
+        float  $storageFee
+    ): void
+    {
+        $pricingRule = PricingRule::factory()->state([
+            'vehicle_type' => $vehicleType,
+            'buyer_fee_percentage' => $buyerFeePercentage,
+            'min_buyer_fee' => $minBuyerFee,
+            'max_buyer_fee' => $maxBuyerFee,
+            'seller_fee_percentage' => $sellerFeePercentage,
+            'association_fee' => $associationFee,
+            'storage_fee' => $storageFee
+        ])->make();
+
+        $this->pricingRuleRepoMock->expects($this->once())
+            ->method('getPricingRules')
+            ->with($vehicleType)
+            ->willReturn($pricingRule);
     }
 }
